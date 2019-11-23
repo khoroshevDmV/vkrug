2eaf471c224dde9a946042437d19a6abe3631eaa
khoroshevDmV <aweman58@gmail.com>
2019-11-23 23:30:23 +0300

    qwerty

--- js/person_data.js
+++ js/person_data.js
@@ -1,5 +1,4 @@
 function data_person_email() {
 	let string_url = window.location.href;
 	person_email.innerHTML = String(String(string_url.split("=")[1]).split(".")[0]);
-	person_password.innerHTML = window.location.href;
 }
