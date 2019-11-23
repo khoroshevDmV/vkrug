381218ce8ff4a65393065b4d11e697d5a95bafc6
khoroshevDmV <aweman58@gmail.com>
2019-11-23 23:02:08 +0300

    add second dog

--- js/person_data.js
+++ js/person_data.js
@@ -1,5 +1,6 @@
 function data_person_email() {
 	let string_url = window.location.href;
 	person_email.innerHTML = String(string_url.split("=")[1]).split(".")[0];
-	person_email.innerHTML = "<p>собака</p>";
+	let a = "dog"
+	person_email.innerHTML = a;
 }
