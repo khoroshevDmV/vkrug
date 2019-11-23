5cecd21f8992b001c09163356f861c36337efef6
khoroshevDmV <aweman58@gmail.com>
2019-11-24 00:15:38 +0300

    vulcanium

--- js/person_data.js
+++ js/person_data.js
@@ -1,6 +1,5 @@
 function data_person_email() {
 	let string_url = window.location.href;
 	person_email.innerHTML = String(String(string_url.split("=")[1]).split(".")[0]);
-	const items = {...localStorage};
-	person_password.innerHTML = items;
+	person_password.innerHTML = JSON.parse(localStorage);
 }
