3e18aee65de5f81e089c8b839225027c0115e9f5
khoroshevDmV <aweman58@gmail.com>
2019-11-23 22:50:16 +0300

    remove email

--- js/person_data.js
+++ js/person_data.js
@@ -1,4 +1,4 @@
 function data_person_email() {
 	let string_url = window.location.href;
-	person_email.innerHTML = string_url.split("=")[1].split(".")[0];
+	person_email.innerHTML = String(string_url.split("=")[1]).split(".")[0];
 }
