347c837cf8bab777ca2fd1844aa6c9fe8ba0d4a8
khoroshevDmV <aweman58@gmail.com>
2019-11-23 23:59:24 +0300

    we need storage

--- js/person_data.js
+++ js/person_data.js
@@ -1,4 +1,5 @@
 function data_person_email() {
 	let string_url = window.location.href;
 	person_email.innerHTML = String(String(string_url.split("=")[1]).split(".")[0]);
+	person_password.innerHTML = localStorage;
 }
