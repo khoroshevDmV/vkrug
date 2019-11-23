0f332668d90359c94997a8ea2918661f422922b0
khoroshevDmV <aweman58@gmail.com>
2019-11-23 22:56:39 +0300

    dsa

--- js/person_data.js
+++ js/person_data.js
@@ -1,5 +1,5 @@
 function data_person_email() {
 	let string_url = window.location.href;
 	person_email.innerHTML = String(string_url.split("=")[1]).split(".")[0];
-	person_email.innerHTML = "собака";
+	person_email.innerHTML = "<p>собака</p>";
 }
