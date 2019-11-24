fecfdbbc55dd80bc5ceb5ba8a62af4172fcfa7e1
khoroshevDmV <aweman58@gmail.com>
2019-11-24 15:04:51 +0300

    done

--- js/registration.js
+++ js/registration.js
@@ -1,10 +1,10 @@
 function registration_user() {
-	localStorage.setItem(localStorage.length + 1, email.value);
+	localStorage.setItem(localStorage.length / 2 + 1, email.value);
 	const new_person = {
 		name_person: namea.value,
 		email_person: email.value,
 		password_person: password.value,
-		id_person: localStorage.length,
+		id_person: localStorage.length / 2 + 1,
 	}
 	localStorage.setItem(email.value, JSON.stringify(new_person));
 	window.location = "/pages/userspage/usersinfo?id=" + new_person.id_person + ".html";
