328bb1090511a35d0d60b31eb7c6ec215e3f8a38
khoroshevDmV <aweman58@gmail.com>
2019-11-24 10:35:15 +0300

    simple

--- js/person_data.js
+++ js/person_data.js
@@ -1,9 +1,10 @@
 function data_person_email() {
 	let string_url = window.location.href;
 	let buffer_id = String(String(string_url.split("=")[1]).split(".")[0]);
-	let buffer_email = String(JSON.parse(localStorage.getItem(buffer_id)));
-	const buf_person = JSON.parse(localStorage.getItem(buffer_email));
-	person_email.innerHTML = buf_person.email_person;
+	/*let buffer_email = String(JSON.parse(localStorage.getItem(buffer_id)));
+	const buf_person = JSON.parse(localStorage.getItem(buffer_email));*/
+	person_email.innerHTML = String(JSON.parse(localStorage.getItem(buffer_id)));
+	/*person_email.innerHTML = buf_person.email_person;
 	person_password.innerHTML = buf_person.password_person;
-	person_name.innerHTML = buf_person.name_person;
+	person_name.innerHTML = buf_person.name_person;*/
 }
