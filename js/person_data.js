746d4a092291b5647b76d880fcceae18206c2c87
khoroshevDmV <aweman58@gmail.com>
2019-11-24 11:50:01 +0300

    papa

--- js/person_data.js
+++ js/person_data.js
@@ -3,13 +3,6 @@ function data_person_email() {
 	let buffer_id = String(String(string_url.split("=")[1]).split(".")[0]);
 	let buffer_email = localStorage.getItem(buffer_id);
 	const buf_person = JSON.parse(localStorage.getItem(buffer_email));
-	/*if (localStorage.getItem(buffer_id)){
-		console.log(localStorage.getItem(buffer_id));
-		console.log("do");
-	} else {
-		console.log(localStorage.getItem(buffer_id));
-		console.log(typeof(localStorage.getItem(buffer_id)));
-	}*/
 	person_email.innerHTML = buf_person.email_person;
 	person_password.innerHTML = buf_person.password_person;
 	person_name.innerHTML = buf_person.name_person;
