b7cbac7d62892ef6b96c04ff9727d1cd46f854c1
khoroshevDmV <aweman58@gmail.com>
2019-11-24 10:00:11 +0300

    fix one bug

--- js/person_data.js
+++ js/person_data.js
@@ -1,6 +1,6 @@
 function data_person_email() {
 	let string_url = window.location.href;
-	let buffer_id = String(String(string_url.split("=")[1]).split(".")[0])
+	let buffer_id = String(String(string_url.split("=")[1]).split(".")[0]);
 	let buffer_email = localStorage.getItem(buffer_id);
 	person_email.innerHTML = buffer_email;
 	person_password.innerHTML = JSON.parse(localStorage.getItem(buffer_email)).password_person;
