fea639d0303bce6cd0b6543fe104421161623510
khoroshevDmV <aweman58@gmail.com>
2019-11-24 10:13:46 +0300

    pot

--- js/person_data.js
+++ js/person_data.js
@@ -3,6 +3,6 @@ function data_person_email() {
 	let buffer_id = String(String(string_url.split("=")[1]).split(".")[0]);
 	let buffer_email = String(localStorage.getItem(buffer_id));
 	person_email.innerHTML = buffer_email;
-	person_password.innerHTML = JSON.parse(localStorage.getItem(buffer_email)).password_person;
-	person_name.innerHTML = JSON.parse(localStorage.getItem(buffer_email)).name_person;
+	person_password.innerHTML = String(JSON.parse(localStorage.getItem(buffer_email)).password_person);
+	person_name.innerHTML = String(JSON.parse(localStorage.getItem(buffer_email)).name_person);
 }
