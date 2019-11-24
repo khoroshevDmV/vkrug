8a86dbda01b1d584444c6685e937d8350aa7ac63
khoroshevDmV <aweman58@gmail.com>
2019-11-24 11:09:20 +0300

    string

--- js/person_data.js
+++ js/person_data.js
@@ -1,6 +1,6 @@
 function data_person_email() {
 	let string_url = window.location.href;
-	let buffer_id = String(String(string_url.split("=")[1]).split(".")[0]);
+	String buffer_id = String(String(string_url.split("=")[1]).split(".")[0]);
 	/*let buffer_email = String(JSON.parse(localStorage.getItem(buffer_id)));
 	const buf_person = JSON.parse(localStorage.getItem(buffer_email));*/
 	if (localStorage.getItem(buffer_id)){
