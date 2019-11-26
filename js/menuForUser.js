a1dbae30652fc9e19eb4a04b96ba2dbe9717d165
khoroshevDmV <aweman58@gmail.com>
2019-11-27 00:20:32 +0300

    create new file js

---
+++ js/menuForUser.js
@@ -0,0 +1,7 @@
+var menu_Exit_a = document.createElement('a');
+var menu_Exit_li = document.createElement('li');
+a.id = "out";
+a.innerHTML = "Выход";
+li.className = "button_out";
+document.nav.ul.append(li);
+document.nav.ul.getElementsByTagName('button_out').append(a);
