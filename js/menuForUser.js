1118044cfcf5c3c2a883247a251706942aecc40c
khoroshevDmV <aweman58@gmail.com>
2019-11-27 00:27:25 +0300

    add

--- js/menuForUser.js
+++ js/menuForUser.js
@@ -1,7 +1,7 @@
 var menu_Exit_a = document.createElement('a');
 var menu_Exit_li = document.createElement('li');
-a.id = "out";
-a.innerHTML = "Выход";
-li.className = "button_out";
-document.nav.ul.append(li);
-document.nav.ul.getElementsByTagName('button_out').append(a);
+menu_Exit_a.id = "out";
+menu_Exit_a.innerHTML = "Выход";
+menu_Exit_li.className = "button_out";
+document.nav.ul.append(menu_Exit_li);
+document.nav.ul.getElementsByTagName('button_out').append(menu_Exit_a);
