4cf884f1fed8a0a0b0101b509f236888376c78db
khoroshevDmV <aweman58@gmail.com>
2019-12-07 00:14:48 +0300

    hidden

--- js/person_data.js
+++ js/person_data.js
@@ -77,9 +77,9 @@ function save_color(bgcolor,txtcolor) {
 	body.style.color = txtcolor;
 }
 button.onclick = function() {
-	if (settings.style.visibility != "hidden") {
-		settings.style.visibility = "hidden";
+	if (settings.hidden) {
+		settings.hidden = false;
 	} else {
-		settings.style.visibility = "visible";
+		settings.hidden = true;
 	}
 }
